474ec52eaaab5a81470f26350a9d8ee291b7fc3c
Joshua Brazier <josh.d.brazier@gmail.com>
2018-11-30 18:07:35 -0600

    Started adding automatic term width detection.

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ INC='-Iinclude'
 default: timage
 
 timage: main.c
-	gcc -std=c99 -Wall $(LIB) $(INC) -o timage main.c -lm
+	gcc -std=c99 -Wall $(LIB) $(INC) -o timage main.c -lm -lncurses
 
 clean:
 	rm -r *.o timage
--- main.c
+++ main.c
@@ -1,20 +1,22 @@
 /*******************************************************************************
- * 
+ *
  *      AUTHOR: Joshua Brazier
- * 
+ *
  *        DATE: 10/14/2018
- * 
+ *
  *      OBJECT: timage
- * 
+ *
  *    FILENAME: main.c
- * 
+ *
  * DESCRIPTION: Timage (from terminal image) is a terminal based image viewer.
- * 
+ *
  ******************************************************************************/
 #include <math.h>
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <signal.h>
 
 #define STB_IMAGE_IMPLEMENTATION // _____ Must be in this order
 #include "stb_image/stb_image.h" // __|
@@ -34,6 +36,7 @@ typedef struct {
 } image_t;
 
 // Prototypes
+void term_resize_handler(int sig);
 void unpack_image(image_t img, unsigned char * img_pkg, int size);
 image_t downsmaple_image(int x_size, int y_size, image_t in_img);
 
@@ -47,8 +50,8 @@ int main(int argc, char* argv[])
     char filename[256];
     image_t img;
     image_t img_resized;
-    
-    if (argc == 1)
+
+    if (argc == 2)
     {
         strncpy(filename, argv[1], 256);
         filename[256] = '\0';
@@ -62,42 +65,69 @@ int main(int argc, char* argv[])
     else
     {
         // TODO: Make this print the help info instead
-        strcpy(filename, "test_images/Lenna_Test_Image.png");
+        strcpy(filename, "./test_images/Lenna_Test_Image.png");
     }
-    
-    data = stbi_load(filename, &x, &y, &n, 0);
-    
-    img.color = (color_t **) malloc(sizeof(color_t *) * y);
-    for (int i = 0; i < y; ++i)
-    {
-        img.color[i] = (color_t *) malloc(sizeof(color_t) * x);
-    }
-    img.size_x = x;
-    img.size_y = y;
-    img.channels = n;
-    
-    unpack_image(img, data, (x*y*n));
-    img_resized = downsmaple_image(2 * height_in, height_in, img);
-    
-    for (int i = 0; i < height_in; ++i)
-    {
-        for (int j = 0; j < 2 * height_in; ++j)
+
+    // Ncurses setup
+    initscr();
+    signal(SIGWINCH, term_resize_handler);
+    while(getch() != '\n') {
+        data = stbi_load(filename, &x, &y, &n, 0);
+
+        img.color = (color_t **) malloc(sizeof(color_t *) * y);
+        for (int i = 0; i < y; ++i)
         {
-            printf("\033[38;2;%u;%u;%um%c", img_resized.color[i][j].r, img_resized.color[i][j].g, img_resized.color[i][j].b, (char) 35);
+            img.color[i] = (color_t *) malloc(sizeof(color_t) * x);
         }
-        
-        printf("\n");
+        img.size_x = x;
+        img.size_y = y;
+        img.channels = n;
+
+        unpack_image(img, data, (x*y*n));
+        img_resized = downsmaple_image(2 * height_in, height_in, img);
+
+        for (int i = 0; i < height_in; ++i)
+        {
+            for (int j = 0; j < 2 * height_in; ++j)
+            {
+                printf("\033[38;2;%u;%u;%um%c", img_resized.color[i][j].r, img_resized.color[i][j].g, img_resized.color[i][j].b, (char) 35);
+            }
+
+            printf("\n");
+        }
     }
-    
+    printf("Width = %d, Height = %d\n", COLS, LINES);
+    endwin();
     stbi_image_free(data);
-    
     return 0;
+}
+
+void term_resize_handler(int sig)
+{
+    int x, y;
+    char tmp[128];
+    signal(SIGWINCH, SIG_IGN);
+
+    endwin();
+    initscr();
+    refresh();
+    clear();
+
+    sprintf(tmp, "%dx%d", COLS, LINES);
+
+    x = COLS / 2 - strlen(tmp) / 2;
+    y = LINES / 2 - 1;
+
+    mvaddstr(y, x, tmp);
+    refresh();
+
+    signal(SIGWINCH, term_resize_handler);
 }
 
 void unpack_image(image_t img, unsigned char * img_pkg, int size)
 {
     int k = 0;
-    
+
     for (int i = 0; i < img.size_y; ++i)
     {
         for (int j = 0; j < img.size_x; ++j)
@@ -105,7 +135,7 @@ void unpack_image(image_t img, unsigned char * img_pkg, int size)
             img.color[i][j].r = (uint8_t) img_pkg[k];
             img.color[i][j].g = (uint8_t) img_pkg[k+1];
             img.color[i][j].b = (uint8_t) img_pkg[k+2];
-            
+
             k += 3;
         }
     }
@@ -116,7 +146,7 @@ image_t downsmaple_image(int x_size, int y_size, image_t in_img)
     double pixel_w = (double) in_img.size_x / (double) x_size;
     double pixel_h = (double) in_img.size_y / (double) y_size;
     image_t ret;
-    
+
     ret.size_x = x_size;
     ret.size_y = y_size;
     ret.channels = in_img.channels;
@@ -125,7 +155,7 @@ image_t downsmaple_image(int x_size, int y_size, image_t in_img)
     {
         ret.color[i] = (color_t *) malloc(sizeof(color_t) * x_size);
     }
-    
+
     for (int y = 0; y < y_size; ++y)
     {
         for (int x = 0; x < x_size; ++x)
@@ -141,7 +171,7 @@ image_t downsmaple_image(int x_size, int y_size, image_t in_img)
             int y_next  = (int) round(pixel_h * ((double) y+1) );
             //int pixel_w_int = x_end - x_start;
             //int pixel_h_int = y_end - y_start;
-            
+
             for (int y_sp = y_start; y_sp < y_next; ++y_sp)
             {
                 for (int x_sp = x_start; x_sp < x_next; ++x_sp)
@@ -152,12 +182,12 @@ image_t downsmaple_image(int x_size, int y_size, image_t in_img)
                     ++num;
                 }
             }
-            
+
             ret.color[y][x].r = (uint8_t) round((double) red   / (double) num);
             ret.color[y][x].g = (uint8_t) round((double) green / (double) num);
             ret.color[y][x].b = (uint8_t) round((double) blue  / (double) num);
         }
     }
-    
+
     return ret;
 }
