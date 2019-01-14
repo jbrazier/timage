282eab6be4bce3758e91b34cd060606a2b74c3b7
Joshua Brazier <josh.d.brazier@gmail.com>
2019-01-13 19:00:24 -0600

    Added command line options, a license, and some improvements.

--- main.c
+++ main.c
@@ -1,22 +1,45 @@
 /*******************************************************************************
  *
- *      AUTHOR: Joshua Brazier
+ *         AUTHOR: Joshua Brazier
  *
- *        DATE: 10/14/2018
+ *           DATE: 10/14/2018
  *
- *      OBJECT: timage
+ *         OBJECT: timage
  *
- *    FILENAME: main.c
+ *       FILENAME: main.c
  *
- * DESCRIPTION: Timage (from terminal image) is a terminal based image viewer.
+ *    DESCRIPTION: Timage (from terminal image) is a terminal based image viewer.
+ * 
+ *      COPYRIGHT: Copyright 2018, 2019 Joshua Brazier
+ * 
+ * LICENSE NOTICE: This file is part of Timage.
+ * 
+ *                 Timage is free software: you can redistribute it and/or modify
+ *                 it under the terms of the GNU General Public License as published by
+ *                 the Free Software Foundation, either version 3 of the License, or
+ *                 (at your option) any later version.
+ *                 
+ *                 Timage is distributed in the hope that it will be useful,
+ *                 but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *                 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *                 GNU General Public License for more details.
+ *                 
+ *                 You should have received a copy of the GNU General Public License
+ *                 along with Timage.  If not, see <https://www.gnu.org/licenses/>.
+ *                 
+ *                 See the file COPYING for more details.
  *
  ******************************************************************************/
 #include <math.h>
 #include <ncurses.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define STB_IMAGE_IMPLEMENTATION // _____ Must be in this order
 #include "stb_image/stb_image.h" // __|
@@ -33,48 +56,148 @@ typedef struct {
     int size_x;
     int size_y;
     int channels;
+    double ratio;
 } image_t;
 
+const char version_string[] = "1.0.0";
+
 // Prototypes
+image_t downsmaple_image(int x_size, int y_size, image_t in_img);
+bool file_is_valid(const char * path);
+void print_copyright(void);
+void print_help(void);
 void term_resize_handler(int sig);
 void unpack_image(image_t img, unsigned char * img_pkg, int size);
-image_t downsmaple_image(int x_size, int y_size, image_t in_img);
 
 int main(int argc, char* argv[])
 {
-    int x;
-    int y;
-    int n;
-    int height_in = 50;
-    unsigned char * data;
-    char filename[256];
-    image_t img;
-    image_t img_resized;
-
-    if (argc == 2)
+    int x;                  // Width of the raw image
+    int y;                  // Height of the raw image
+    int n;                  // Number of color channels in the raw image
+    int printed_img_height; // Height of the image output to the terminal
+    int printed_img_width;  // Width of the image output to the terminal
+    int height_in = 0;      // Custom height specified
+    int TERM_LINES;         // Set to terminal height or user specified height
+    unsigned char * data;   // Raw image data
+    char filename[256];     // Image location
+    image_t img;            // Better image container
+    image_t img_resized;    // Resized image for printing to terminal
+    double term_ratio;      // Aspect ratio of the terminal
+    struct stat st;         // For checking input file validity
+    
+    // Handle command line arguments
+    // TODO: Refactor this
+    if (argc == 1)
     {
-        strncpy(filename, argv[1], 256);
-        filename[256] = '\0';
+        print_help();
+        printf("\n");
+        print_copyright();
+        return 0;
     }
-    else if (argc > 2)
+    else if (argc == 2)
     {
-        strncpy(filename, argv[1], 256);
-        filename[256] = '\0';
-        height_in = atoi(argv[2]);
+        if (strcmp(argv[1], "-h"    ) == 0 ||
+            strcmp(argv[1], "--help") == 0 )
+        {
+            print_help();
+            printf("\n");
+            print_copyright();
+            return 0;
+        }
+        else if (strcmp(argv[1], "-v"       ) == 0 ||
+                 strcmp(argv[1], "--version") == 0 )
+        {
+            printf("Timage v%s\n", version_string);
+            print_copyright();
+            return 0;
+        }
+        else
+        {
+            strncpy(filename, argv[1], 256);
+            filename[256] = '\0';
+            
+            if (!file_is_valid(filename))
+            {
+                printf("Could not access input file.\n");
+                printf("Try \"%s --help\" for usage.\n", argv[0]);
+            }
+        }
     }
-    else
+    else if (argc == 3)
     {
-        // TODO: Make this print the help info instead
-        strcpy(filename, "./test_images/Lenna_Test_Image.png");
+        if (strcmp(argv[1], "-h"    ) == 0 ||
+            strcmp(argv[1], "--help") == 0 )
+        {
+            print_help();
+            printf("\n");
+            print_copyright();
+            return 0;
+        }
+        else if (strcmp(argv[1], "-v"       ) == 0 ||
+            strcmp(argv[1], "--version") == 0 )
+        {
+            printf("Timage v%s\n", version_string);
+            print_copyright();
+            return 0;
+        }
+        else
+        {
+            strncpy(filename, argv[1], 256);
+            filename[256] = '\0';
+            
+            if (!file_is_valid(filename))
+            {
+                printf("Could not access input file.\n");
+                printf("Try \"%s --help\" for usage.\n", argv[0]);
+            }
+            
+            height_in = atoi(argv[2]);
+        }
+    }
+    else if (argc >= 4)
+    {
+        if (strcmp(argv[1], "-h"    ) == 0 ||
+            strcmp(argv[1], "--help") == 0 )
+        {
+            print_help();
+            printf("\n");
+            print_copyright();
+            return 0;
+        }
+        else if (strcmp(argv[1], "-v"       ) == 0 ||
+            strcmp(argv[1], "--version") == 0 )
+        {
+            printf("Timage v%s\n", version_string);
+            print_copyright();
+            return 0;
+        }
+        else
+        {
+            printf("\"%s\" is an invalid option.\n\n", argv[1]);
+            strncpy(filename, argv[2], 256);
+            filename[256] = '\0';
+            
+            if (!file_is_valid(filename))
+            {
+                printf("Could not access input file.\n");
+                printf("Try \"%s --help\" for usage.\n", argv[0]);
+            }
+            
+            height_in = atoi(argv[3]);
+        }
     }
 
     // Ncurses setup
     initscr();
     signal(SIGWINCH, term_resize_handler);
     endwin();
+    
+    TERM_LINES = (height_in > 0) ? ceil((height_in + 2.0) / 2.0) : LINES + 1;
 
+    // Get the raw image data
     data = stbi_load(filename, &x, &y, &n, 0);
 
+    // Allocate space for the image in a better container
     img.color = (color_t **) malloc(sizeof(color_t *) * y);
     for (int i = 0; i < y; ++i)
     {
@@ -83,23 +206,118 @@ int main(int argc, char* argv[])
     img.size_x = x;
     img.size_y = y;
     img.channels = n;
+    img.ratio = (double) x / (double) y; // >1 landscape, <1 portrait, =1 square
+    term_ratio = (double) COLS / (double) ((TERM_LINES - 1) * 2);
 
+    // Unpack and downsample the image so it fits the correct number of "pixels"
     unpack_image(img, data, (x*y*n));
-    img_resized = downsmaple_image(2 * (LINES-1), (LINES-1), img);
-
-    for (int i = 0; i < (LINES-1); ++i)
+    if (term_ratio >= img.ratio)
     {
-        for (int j = 0; j < 2 * (LINES-1); ++j)
+        int height = (TERM_LINES-1);
+        int width = (int) floor((double) (2 * height) * img.ratio);
+        img_resized = downsmaple_image(width, height, img);
+    }
+    else
+    {
+        int width = COLS;
+        int height = (int) floor((double) COLS / (2.0 * img.ratio));
+        img_resized = downsmaple_image(width, height, img);
+    }
+
+    // Print the resized image to the terminal
+    for (int i = 0; i < img_resized.size_y; ++i)
+    {
+        for (int j = 0; j < img_resized.size_x; ++j)
         {
             printf("\033[38;2;%u;%u;%um\u2588", img_resized.color[i][j].r, img_resized.color[i][j].g, img_resized.color[i][j].b);
         }
 
         printf("\n");
     }
-
-    //printf("Width = %d, Height = %d\n", COLS, LINES);
+    printf("\033[0m");
+    
+    // Cleanup and return
     stbi_image_free(data);
     return 0;
+}
+
+image_t downsmaple_image(int x_size, int y_size, image_t in_img)
+{
+    double pixel_w = (double) in_img.size_x / (double) x_size;
+    double pixel_h = (double) in_img.size_y / (double) y_size;
+    image_t ret;
+    
+    ret.ratio = (double) x_size / (double) y_size;
+    ret.size_x = x_size;
+    ret.size_y = y_size;
+    ret.channels = in_img.channels;
+    ret.color = (color_t **) malloc(sizeof(color_t *) * y_size);
+    for (int i = 0; i < y_size; ++i)
+    {
+        ret.color[i] = (color_t *) malloc(sizeof(color_t) * x_size);
+    }
+    
+    for (int y = 0; y < y_size; ++y)
+    {
+        for (int x = 0; x < x_size; ++x)
+        {
+            // TODO: Make this work with images other than 3 channel images
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+            int num = 0;
+            int x_start = (int) round(pixel_w *  (double) x    );
+            int y_start = (int) round(pixel_h *  (double) y    );
+            int x_next  = (int) round(pixel_w * ((double) x+1) );
+            int y_next  = (int) round(pixel_h * ((double) y+1) );
+            //int pixel_w_int = x_end - x_start;
+            //int pixel_h_int = y_end - y_start;
+            
+            for (int y_sp = y_start; y_sp < y_next; ++y_sp)
+            {
+                for (int x_sp = x_start; x_sp < x_next; ++x_sp)
+                {
+                    red   += in_img.color[y_sp][x_sp].r;
+                    green += in_img.color[y_sp][x_sp].g;
+                    blue  += in_img.color[y_sp][x_sp].b;
+                    ++num;
+                }
+            }
+            
+            ret.color[y][x].r = (uint8_t) round((double) red   / (double) num);
+            ret.color[y][x].g = (uint8_t) round((double) green / (double) num);
+            ret.color[y][x].b = (uint8_t) round((double) blue  / (double) num);
+        }
+    }
+    
+    return ret;
+}
+
+bool file_is_valid(const char * path)
+{
+    struct stat st;
+    
+    return (stat(path, &st) >= 0);
+}
+
+void print_copyright(void)
+{
+    printf("Timage  Copyright (C) 2018, 2019  Joshua Brazier\n");
+    printf("This program comes with ABSOLUTELY NO WARRANTY.\n");
+    printf("This is free software, and you are welcome to redistribute it\n");
+    printf("under certain conditions; see the file COPYING for details.\n");
+}
+
+void print_help(void)
+{
+    printf("Usage: timage [OPTION] [FILE] [HEIGHT]\n");
+    printf("Displays an image file in the terminal output.\n");
+    printf("\nOPTIONS:\n");
+    printf("  -h, --help\t\tDisplay this help and exit\n");
+    printf("  -v, --version\t\tOutput version information and exit.\n");
+    printf("\nThe HEIGHT argument can be specified to manually set the height\n");
+    printf("of the output image (the width is determined by the images aspect\n");
+    printf("ratio.\n");
 }
 
 void term_resize_handler(int sig)
@@ -141,53 +359,4 @@ void unpack_image(image_t img, unsigned char * img_pkg, int size)
     }
 }
 
-image_t downsmaple_image(int x_size, int y_size, image_t in_img)
-{
-    double pixel_w = (double) in_img.size_x / (double) x_size;
-    double pixel_h = (double) in_img.size_y / (double) y_size;
-    image_t ret;
 
-    ret.size_x = x_size;
-    ret.size_y = y_size;
-    ret.channels = in_img.channels;
-    ret.color = (color_t **) malloc(sizeof(color_t *) * y_size);
-    for (int i = 0; i < y_size; ++i)
-    {
-        ret.color[i] = (color_t *) malloc(sizeof(color_t) * x_size);
-    }
-
-    for (int y = 0; y < y_size; ++y)
-    {
-        for (int x = 0; x < x_size; ++x)
-        {
-            // TODO: Make this work with images other than 3 channel images
-            int red = 0;
-            int green = 0;
-            int blue = 0;
-            int num = 0;
-            int x_start = (int) round(pixel_w *  (double) x    );
-            int y_start = (int) round(pixel_h *  (double) y    );
-            int x_next  = (int) round(pixel_w * ((double) x+1) );
-            int y_next  = (int) round(pixel_h * ((double) y+1) );
-            //int pixel_w_int = x_end - x_start;
-            //int pixel_h_int = y_end - y_start;
-
-            for (int y_sp = y_start; y_sp < y_next; ++y_sp)
-            {
-                for (int x_sp = x_start; x_sp < x_next; ++x_sp)
-                {
-                    red   += in_img.color[y_sp][x_sp].r;
-                    green += in_img.color[y_sp][x_sp].g;
-                    blue  += in_img.color[y_sp][x_sp].b;
-                    ++num;
-                }
-            }
-
-            ret.color[y][x].r = (uint8_t) round((double) red   / (double) num);
-            ret.color[y][x].g = (uint8_t) round((double) green / (double) num);
-            ret.color[y][x].b = (uint8_t) round((double) blue  / (double) num);
-        }
-    }
-
-    return ret;
-}
